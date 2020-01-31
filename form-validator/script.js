6d8b9baed162d744600ac028301a39705b53312b
tenspot <mervyn.posnett@gmail.com>
2020-01-31 13:37:38 +0000

    new project

--- form-validator/script.js
+++ form-validator/script.js
@@ -19,12 +19,23 @@ function showSuccess(input, message) {
 	formControl.className = 'form-control success';
 }
 
+// // Check email is valid
+// function isValidEmail(email) {
+// 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// 	return re.test(String(email).toLowerCase());
+// }
+
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
+	if (re.test(input.value.trim())) {
+		showSuccess(input);
+	} else {
+		showError(input, 'Invalid email address');
+	}
 }
 
+// Check required fields
 function checkRequire(inputArray) {
 	inputArray.forEach(function(input) {
 		if (input.value.trim() === '') {
@@ -35,15 +46,37 @@ function checkRequire(inputArray) {
 	});
 }
 
+// Check input length
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+	} else if (input.value.length >= max) {
+		showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+	} else {
+		showSuccess(input);
+	}
+}
+
+// Check password match
+function checkPasswordsMatch(input1, input2) {
+	if (input1.value !== input2.value) {
+		showError(input2, 'Passwords do not match');
+	}
+}
+
 // Get field name with first letter upper case
 function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Event listeners
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 	checkRequire([ username, email, password, password2 ]);
+	checkLength(username, 3, 15);
+	checkLength(password, 6, 25);
+	checkEmail(email);
+	checkPasswordsMatch(password, password2);
 
 	// if (username.value === '') {
 	// 	showError(username, 'Username is a required field');
